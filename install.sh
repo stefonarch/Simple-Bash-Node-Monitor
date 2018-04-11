c18d3c0d653a854e5a877640af407b370ee54678
stefonarch <stef.binde@riseup.net>
2018-04-11 14:34:00 +0200

    Fix permissions, add missing scripts

--- install.sh
+++ install.sh
@@ -18,7 +18,8 @@ apt install -y bc nginx
 service nginx start
 systemctl enable nginx.service
 echo "Copying files in /usr/local/bin..."
-cp scripts/* /usr/local/bin
+sleep 3
+cp scripts/* /usr/local/bin/
 # Path to web, edit this if different 
 cp api /var/www/html/api
 cp SimpleBashMonitor /usr/local/bin/
@@ -27,7 +28,9 @@ cp SimpleBashMonitor /usr/local/bin/
 #sed -i 's/"rpc_enable": "false"/"rpc_enable": "true"/g' RaiBlocks/config.json
 #sed -i 's/"enable_control": "false"/"enable_control": "true"/g' RaiBlocks/config.json
 #sed -i 's/"address": "::1"/"address": "::ffff:127.0.0.1"/g' RaiBlocks/config.json
-echo "Adding a crontab to  run the monitor every minute..."
+echo "Adding a crontab to  refresh the monitor every minute..."
+sleep 3
 (crontab -l 2>/dev/null; echo "*/1 * * * * /usr/local/bin/SimpleBashMonitor") | crontab -
-echo "Setup finished, edit your monitor settings:"
+echo "Setup finished, edit now your monitor settings:"
+sleep 3
 nano /usr/local/bin/SimpleBashMonitor
